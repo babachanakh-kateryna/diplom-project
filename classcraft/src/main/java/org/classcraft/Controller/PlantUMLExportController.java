70937e6a2a503cc62a45a341f3b7fc25fac457e6
Kateryna Babachanakh <katrin.ltvnv@gmail.com>
2025-05-04 12:51:23 +0200

    controller export

---
+++ classcraft/src/main/java/org/classcraft/Controller/PlantUMLExportController.java
@@ -0,0 +1,34 @@
+package org.classcraft.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
+import org.classcraft.Export.ExportPlantUML;
+import org.classcraft.Model.DiagramModel;
+
+import java.io.File;
+
+// Контролер для експорту діаграми у формат PlantUML
+public class PlantUMLExportController extends Controller {
+
+    // Конструктор для ініціалізації контролера
+    public PlantUMLExportController(DiagramModel model) {
+        super(model);
+    }
+
+    // Обробляє подію експорту у файл PlantUML
+    @Override
+    public void handle(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Зберегти файл .puml");
+
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Файли PlantUML (*.puml)", "*.puml");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        // Встановлення імені файлу за замовчуванням
+        fileChooser.setInitialFileName("diagram.puml");
+        File file = fileChooser.showSaveDialog(((MenuItem) event.getSource()).getParentPopup().getScene().getWindow());
+
+        model.export(new ExportPlantUML(file));
+    }
+}
